becd3445601739a863d96323dbc0918b1efa4fcb
Zuthuzu <zuthuzu@gmail.com>
2020-02-16 21:17:21 +0200

    added a test

--- src/test/java/ua/kpi/tef/zu/MainTest.java
+++ src/test/java/ua/kpi/tef/zu/MainTest.java
@@ -1,8 +1,11 @@
 package ua.kpi.tef.zu;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import ua.kpi.tef.zu.controller.TourProperties;
+import ua.kpi.tef.zu.model.*;
 
 /**
  * Unit test for simple App.
@@ -16,5 +19,28 @@ public class MainTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void testModelFilters()
+    {
+        Model model = new Model();
+        Tour tour = new Tour();
+
+        tour.setCountry(Countries.ITALY.toString());
+        tour.setTransport(Transport.PLANE.toString());
+        tour.setGoal(TravelGoals.SHOPPING.toString());
+        tour.setFood(Food.BREAKFAST.toString());
+        tour.setDays("10");
+
+        model.addNewTour(tour);
+
+        model.applyFilter(TourProperties.COUNTRIES, new String[]{Countries.ITALY.toString()});
+        model.applyFilter(TourProperties.TRANSPORT, new String[]{Transport.PLANE.toString()});
+        model.applyFilter(TourProperties.TRAVEL_GOALS, new String[]{TravelGoals.SHOPPING.toString()});
+        model.applyFilter(TourProperties.FOOD, new String[]{Food.BREAKFAST.toString()});
+        model.applyFilter(TourProperties.DAYS, new String[]{"10"});
+
+        assertEquals(1, model.conformsToFilter());
     }
 }
